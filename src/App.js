2d3dfbf5ce0770b3d2cfc70b73628f840b18ecac
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-04-02 16:13:35 -0300

    chore: creation of the function to switch to the next page automatically.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <Router>
       <Initial />
+      <Routes>
+      </Routes>
     </Router>
   );
 }
--- src/components/pages/initial.js
+++ src/components/pages/initial.js
@@ -1,8 +1,19 @@
 import { Container } from "@mui/material";
 import styles from "./Initial.module.css";
 import logo from "../img/logo_cl.png";
+import { useNavigate } from "react-router-dom";
 
 function Initial() {
+  const navigator = useNavigate();
+
+  function NextPage() {
+    setTimeout(() => {
+      navigator("/login");
+    }, 2000);
+  }
+
+  NextPage();
+
   return (
     <Container className={styles.init}>
       <div>
