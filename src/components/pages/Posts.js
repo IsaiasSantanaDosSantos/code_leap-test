f2f5b6fee8cf55475a03fa69d746eef9cfc3e5d5
Isaias Santana dos Santos <65987820+IsaiasSantanaDosSantos@users.noreply.github.com>
2022-04-10 15:42:48 -0300

    Update Posts.js

--- src/components/pages/Posts.js
+++ src/components/pages/Posts.js
@@ -1,188 +1,134 @@
-import {
-  Button,
-  Container,
-  inputLabelClasses,
-  outlinedInputClasses,
-  TextField,
-} from "@mui/material";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import styles from "./Posts.module.css";
-import { useSelector } from "react-redux";
-import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import EditIcon from "@material-ui/icons/Edit";
-import { useState } from "react";
-import styled from "@emotion/styled";
-
-const StyledTextField = styled(TextField)({
-  [`& .${outlinedInputClasses.root} .${outlinedInputClasses.notchedOutline}`]: {
-    borderColor: "#777777",
-  },
-  [`& .${outlinedInputClasses.root}.${outlinedInputClasses.focused} .${outlinedInputClasses.notchedOutline}`]:
-    {
-      borderColor: "#777777",
-    },
-  [`&:hover .${outlinedInputClasses.input}`]: {
-    color: "#000000",
-  },
-  [`& .${outlinedInputClasses.root}.${outlinedInputClasses.focused} .${outlinedInputClasses.input}`]:
-    {
-      color: "#777777",
-    },
-  [`& .${inputLabelClasses.outlined}`]: {
-    color: "#000000",
-  },
-  [`&:hover .${inputLabelClasses.outlined}`]: {
-    color: "#000000",
-  },
-  [`& .${inputLabelClasses.outlined}.${inputLabelClasses.focused}`]: {
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: "16px",
-    lineHeight: "19px",
-    color: "#000000",
-  },
-});
-
-function Post() {
-  const postList = useSelector((state) => state.postList);
-  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [isEditModal, setIsEditModal] = useState(false);
-
-  const postOwner = useSelector((state) => state.user.name);
-
-  const deletePost = () => {
-    setIsDeleteModalVisible(true);
-  };
-
-  const confirmDelete = () => {
-    setIsDeleteModalVisible(false);
-  };
-
-  const editPost = () => {
-    setIsEditModal(true);
-  };
-
-  const saveEdit = () => {
-    setIsEditModal(false);
-  };
-  return (
-    <div>
-      <div>
-        {postList.map((idPost) => (
-          <form key={idPost.idPost} className={styles.postsForm}>
-            <div className={styles.containerTitle}>
-              <div className={styles.containerPostTitle}>
-                <h4 className={styles.post_Title}>{idPost.titlePost}</h4>
-              </div>
-              {postOwner === idPost.author ? (
-                <div className={styles.containerIcons}>
-                  <div className={styles.iconsClass}>
-                    <DeleteForeverIcon onClick={deletePost} />
-                  </div>
-                  <div className={styles.iconsClass}>
-                    <EditIcon onClick={editPost} />
-                  </div>
-                </div>
-              ) : null}
-            </div>
-            <div className={styles.containernameAndTime}>
-              <div className={styles.containerAuthorName}>
-                {<p>@{idPost.author}</p>}
-              </div>
-              <div className={styles.timePost}>
-                <p>{idPost.date}</p>
-              </div>
-            </div>
-            <Container>
-              <TextareaAutosize
-                disabled
-                aria-label="minimum height"
-                minRows={8}
-                style={{
-                  width: "100%",
-                  background: "#FFFFFF",
-                  border: "1px solid #777777",
-                  boxSizing: "borderBox",
-                  borderRadius: "4px",
-                  padding: "10px",
-                  fontFamily: "Roboto",
-                  fontStyle: "normal",
-                  fontWeight: "400px",
-                  fontSize: "16px",
-                  lineHeight: "16px",
-                  color: "#000000",
-                }}
-                value={idPost.postContent}
-              />
-            </Container>
-          </form>
-        ))}
-      </div>
-      {isDeleteModalVisible ? (
-        <div className={styles.bodyDeleteModal}>
-          <div className={styles.deleteModal}>
-            <div className={styles.deleteConfirmation}>
-              <p>Are you sure you want to delete this item?</p>
-            </div>
-            <div className={styles.deteleBtnBox}>
-              <div className={styles.confirmationBtn}>
-                <Button onClick={() => setIsDeleteModalVisible(false)}>
+função  Post ( )  {
+  const  postList  =  useSelector ( ( state )  =  > state.postList ) ; _
+  const  [ isDeleteModalVisible ,  setIsDeleteModalVisible ]  =  useState ( false ) ;
+  const  [ isEditModal ,  setIsEditModal ]  =  useState ( false ) ;
+  const  postOwner  =  useSelector ( ( estado )  =  > estado.usuário.nome ) ; _ _ _
+  const  deletePost  =  ( )  =>  {
+    setIsDeleteModalVisible ( true ) ;
+  } ;
+  const  confirmDelete  =  ( )  =>  {
+    setIsDeleteModalVisible ( false ) ;
+  } ;
+  const  editPost  =  ( )  =>  {
+    setIsEditModal ( true ) ;
+  } ;
+  const  salvarEdit  =  ( )  =>  {
+    setIsEditModal ( false ) ;
+  } ;
+  retornar  (
+    < div >
+      < div >
+        { postLista . mapa ( ( idPost )  =>  (
+          < chave de formulário  = { idPost . idPost } className = { estilos . postsFormulário } > 
+            < div  className = { estilos . containerTitle } >
+              < div  className = { estilos . containerPostTitle } >
+                < h4  className = { estilos . post_Title } > { idPost . titlePost } < / h4 >
+              < / div >
+              { postOwner  ===  idPost . autor ? (
+                < div  className = { estilos . containerIcons } >
+                  < div  className = { estilos . classe de ícones } >
+                    < DeleteForeverIcon  onClick = { deletePost }  / >
+                  < / div >
+                  < div  className = { estilos . classe de ícones } >
+                    < EditIcon  onClick = { editPost }  / >
+                  < / div >
+                < / div >
+              ) : nulo }
+            < / div >
+            < div  className = { estilos . containernameAndTime } >
+              < div  className = { estilos . containerAuthorName } >
+                { < p > @ { idPost . autor } < / p > }
+              < / div >
+              < div  className = { estilos . horaPost } >
+                < p > { idPost . data } < / p >
+              < / div >
+            < / div >
+            < Contêiner >
+              < TextareaAutosize
+                Desativado
+                aria-label = "altura mínima"
+                minLinhas = { 8 }
+                estilo = { {
+                  largura : "100%" ,
+                  fundo : "#FFFFFF" ,
+                  border : "1px sólido #777777" ,
+                  boxSizing : "borderBox" ,
+                  borderRadius : "4px" ,
+                  preenchimento : "10px" ,
+                  fontFamily : "Roboto" ,
+                  fontStyle : "normal" ,
+                  fontWeight : "400px" ,
+                  fontSize : "16px" ,
+                  lineHeight : "16px" ,
+                  cor : "#000000" ,
+                } }
+                valor = { idPost . postConteúdo }
+              / >
+            < / Contêiner >
+          < / formulário >
+        ) ) }
+      < / div >
+      { isDeleteModalVisible ? (
+        < div  className = { estilos . bodyDeleteModal } >
+          < div  className = { estilos . deleteModal } >
+            < div  className = { estilos . deleteConfirmação } >
+              < p > Tem certeza de que deseja excluir este item? < / p >
+            < / div >
+            < div  className = { estilos . deteleBtnBox } >
+              < div  className = { estilos . confirmaçãoBtn } >
+                < Button  onClick = { ( )  =>  setIsDeleteModalVisible ( false ) } >
                   cancelar
-                </Button>
-                <Button onClick={confirmDelete}>ok</Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
-      {isEditModal ? (
-        <div className={styles.bodyDeleteModal}>
-          <div className={styles.editModal}>
-            <div className={styles.editConfirmation}>
-              <p>Edit item</p>
-            </div>
-            {/*{postList.map((idPost) => (*/}
-              <div >
-                <StyledTextField
-                  fullWidth={true}
-                  variant="outlined"
-                  label="Title"
-                  name="name"
-                  placeholder="Hello world"
-                ></StyledTextField>
-                <TextareaAutosize
-                  aria-label="minimum height"
-                  minRows={8}
-                  placeholder="Content here"
-                  style={{
-                    width: "100%",
-                    marginTop: "20px",
-                    background: "#FFFFFF",
-                    border: "1px solid #777777",
-                    boxSizing: "borderBox",
-                    borderRadius: "4px",
-                    padding: "10px",
-                    fontFamily: "Roboto",
-                    fontStyle: "normal",
-                    fontWeight: "400px",
-                    fontSize: "16px",
-                    lineHeight: "16px",
-                    color: "#000000",
-                  }}
-                  name="postsContent"
-                ></TextareaAutosize>
-              </div>
-            {/*))}*/}
+                < / Botão >
+                < Button  onClick = { confirmDelete } > ok < / Button >
+              < / div >
+            < / div >
+          < / div >
+        < / div >
+      ) : nulo }
+      { éEditModal ? (
+        < div  className = { estilos . bodyDeleteModal } >
+          < div  className = { estilos . editModal } >
+            < div  className = { estilos . editConfirmação } >
+              < p > Editar item < / p >
+            < / div >
+            { /*{postList.map((idPost) => (*/ }
+              < div  >
+                < StyledTextField
+                  fullWidth = { true }
+                  variante = "delineado"
+                  etiqueta = "Título"
+                  nome = "nome"
+                  placeholder = "Olá mundo"
+                > < / StyledTextField >
+                < TextareaAutosize
+                  aria-label = "altura mínima"
+                  minLinhas = { 8 }
+                  placeholder = "Conteúdo aqui"
+                  estilo = { {
+                    largura : "100%" ,
+                    marginTop : "20px" ,
+                    fundo : "#FFFFFF" ,
+                    border : "1px sólido #777777" ,
+                    boxSizing : "borderBox" ,
+                    borderRadius : "4px" ,
+                    preenchimento : "10px" ,
+                    fontFamily : "Roboto" ,
+                    fontStyle : "normal" ,
+                    fontWeight : "400px" ,
+                    fontSize : "16px" ,
+                    lineHeight : "16px" ,
+                    cor : "#000000" ,
+                  } }
+                  nome = "conteúdo das postagens"
+                > < / TextareaAutosize >
+              < / div >
+            { /*))}*/ }
 
-            <div className={styles.saveBtn}>
-              <Button onClick={saveEdit}>save</Button>
-            </div>
-          </div>
-        </div>
-      ) : null}
-    </div>
-  );
+            < div  className = { estilos . saveBtn } >
+              < Button  onClick = { saveEdit } > save < / Button >
+            < / div >
+	@@ -182,4 +185,4 @@ function Post() {
+  ) ;
 }
 
-export default Post;
+exportar  postagem padrão  ;
