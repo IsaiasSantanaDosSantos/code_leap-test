22544b26cfba8e6b8ebcf4f9064693b7fe24e7ba
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-04-12 14:22:25 -0300

    correction

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
-import store from "./redux/Store"
+import store from "./redux/Store";
 
 ReactDOM.render(
   <React.StrictMode>
