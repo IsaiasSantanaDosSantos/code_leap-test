ce0f90277ef05c792096f990987fb2737281a8fc
Isaias Santana dos Santos <65987820+IsaiasSantanaDosSantos@users.noreply.github.com>
2022-04-12 02:48:50 -0300

    Update form.js component

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -1,3 +1,5 @@
+// THIS COMPONENT DON'T IS USE
+
 import styled from "@emotion/styled";
 import {
   inputLabelClasses,
@@ -5,18 +7,9 @@ import {
   TextareaAutosize,
   TextField,
 } from "@mui/material";
-<<<<<<< HEAD
-<<<<<<< HEAD
-//import { useState } from "react";
-=======
-import { useState } from "react";
->>>>>>> parent of 045e7cb (chore: update in the code.)
 import { useSelector } from "react-redux";
-=======
-import { useState } from "react";
->>>>>>> parent of 1a3605c (build: show date in posts.)
 
-import styles from "./Form.module.css"
+import styles from "./form.module.css"
 
 const StyledTextField = styled(TextField)({
   [`& .${outlinedInputClasses.root} .${outlinedInputClasses.notchedOutline}`]: {
@@ -50,9 +43,8 @@ const StyledTextField = styled(TextField)({
 });
 
 function Form() {
-<<<<<<< HEAD
   const postList = useSelector((state) => state.postList);
-  const postOwner = useSelector((state) => state.user.name);
+  //const postOwner = useSelector((state) => state.user.name);
   //const [titlePost, setTitlePost] = useState();
   //const [postContent, setPostContent] = useState();
   //console.log(postList.postOwner)
@@ -94,47 +86,9 @@ function Form() {
         </di>
       ))}
       
-=======
-  const [titlePost, setTitlePost] = useState();
-  const [postContent, setPostContent] = useState();
-
-  return (
-    <div className={styles.newForm}>
-      <StyledTextField
-        fullWidth={true}
-        variant="outlined"
-        label="Title"
-        name="name"
-        placeholder="Hello world"
-        value={titlePost}
-        onChange={(e) => setTitlePost(e.target.value)}
-      />
-      <TextareaAutosize
-        aria-label="minimum height"
-        minRows={8}
-        placeholder="Content here"
-        style={{
-          width: "100%",
-          marginTop: "20px",
-          background: "#FFFFFF",
-          border: "1px solid #777777",
-          boxSizing: "borderBox",
-          borderRadius: "4px",
-          padding: "10px",
-          fontFamily: "Roboto",
-          fontStyle: "normal",
-          fontWeight: "400px",
-          fontSize: "16px",
-          lineHeight: "16px",
-          color: "#000000",
-        }}
-        name="postsContent"
-        value={postContent}
-        onChange={(e) => setPostContent(e.target.value)}
-      />
->>>>>>> parent of 1a3605c (build: show date in posts.)
     </div>
   );
 }
 
 export default Form;
+
